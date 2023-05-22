5e06e6e0fc8740477d7ce5bd2522074b44ec1af1
Leto05 <leto.harm@student.htldornbirn.at>
2023-05-22 12:45:52 +0200

    Interfaces Lernvideos, angefangen. Mitschüler geholfen

---
+++ src/Games/Shapes/ObjectActor.java
@@ -0,0 +1,9 @@
+package Games.Shapes;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+
+public interface ObjectActor {
+    public void render(GameContainer gameContainer, Graphics graphics);
+    public void update(int i);
+}
